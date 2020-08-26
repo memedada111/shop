71626a7dafadee365a11d80d37f8abda1f38f0ae
九现神龙111 <1344559276@qq.com>
2020-08-26 08:22:31 +0800

    新增

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -1,111 +1,148 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+use catetree\Catetree;
 class Cate extends Controller
 {
     public function lst()
     {
-        $cate = db('cate')->order('id desc')->paginate(10);
+        $cate=new Catetree();
+        $cateObj=db('cate');
+        // var_dump($cateObj);die;
+        if(request()->isPost()){
+            $data=input('post.');
 
-        $this->assign('cate',$cate);
+            // var_dump($data);die;
+            $cate->cateSort($data['sort'],$cateObj);
+            $this->success('排序成功！',url('lst'));
+        }
+        $cateRes=$cateObj->order('sort DESC')->select();
+        $cateRes=$cate->catetree($cateRes);
+
+        // var_dump($cateRes);die;
+        $this->assign([
+            'cateRes'=>$cateRes,
+            ]);
         return view('list');
     }
 
     public function add()
     {
-    	if(request()->isPost()){
-    		$data=input('post.');
-    
-    		$add=db('cate')->insert($data);
-    		if($add){
-    			$this->success('添加分类成功！','lst');
-    		}else{
-    			$this->error('添加分类失败！');
-    		}
-    		return;
-    	}
-
-        $res = db('cate')->select();
-
-            $this->assign('res',$res);  
-
+        $cate=new Catetree();
+        $cateObj=db('cate');
+        if(request()->isPost()){
+            $data=input('post.');
+            //判断是否可以添加子分类
+            if(in_array($data['pid'], ['1','3'])){
+                $this->error('系统分类不能作为上级分类！');
+            }
+            //
+            if($data['pid']==2){
+                $data['cate_type']=3;
+            }
+            //
+            $cateId=$cateObj->field('pid')->find($data['pid']);
+            $cateId=$cateId['pid'];
+            if($cateId==2){
+                $this->error('此分类不能作为上级分类！');
+            }
+            //验证
+            $validate = validate('Cate');
+            if(!$validate->check($data)){
+                $this->error($validate->getError());
+            }
+            $add=$cateObj->insert($data);
+            if($add){
+                $this->success('添加分类成功！','lst');
+            }else{
+                $this->error('添加分类失败！');
+            }
+            return;
+        }
+        $cateRes=$cateObj->order('sort DESC')->select();
+        $cateRes=$cate->catetree($cateRes);
+        $this->assign([
+            'cateRes'=>$cateRes,
+            ]);
         return view();
     }
 
     public function edit()
     {
-         if(request()->isPost()){
+        $cate=new Catetree();
+        $cateObj=db('cate');
+        if(request()->isPost()){
             $data=input('post.');
-
-            // dump($data);die;
-
-            if($data['brand_url']&&stripos($data['brand_url'],'http://')===false){
-                $data['brand_url'] = 'http://'.$data['brand_url'];
-
+            //验证
+            $validate = validate('Cate');
+            if(!$validate->check($data)){
+                $this->error($validate->getError());
             }
-            //处理图片上传
-            if($_FILES['brand_img']['tmp_name']){
-
-                $oldBrands = db('brand')->field('brand_img')->find($data['id']);
-              // dump($oldBrands);die;
-             $old = UPLOADS.$oldBrands['brand_img'];
-             // dump($old);die;
-
-             if(file_exists($old)){
-
-                @unlink($old);
-             }
-             
-                $data['brand_img']=$this->upload();
-            }
-            $save = db('brand')->update($data);
-
-              $validate = validate('brand');
-
-             if (!$validate->check($data)) {
-            $this->error($validate->getError());
-        }
-
-            if($save){
-                $this->success('修改品牌成功！','lst');
+            $save=$cateObj->update($data);
+            if($save !== false){
+                $this->success('修改分类成功！','lst');
             }else{
-                $this->error('修改品牌失败！');
+                $this->error('修改分类失败！');
             }
             return;
         }
-        $id = input('id');
-        $brands = db('brand')->find($id);
-          $this->assign('brands',$brands);
+        $cates=$cateObj->find(input('id'));
+        $cateRes=$cateObj->order('sort DESC')->select();
+        $cateRes=$cate->catetree($cateRes);
+        $this->assign([
+            'cateRes'=>$cateRes,
+            'cates'=>$cates
+            ]);
         return view();
     }
 
     public function del($id)
     {
-        $res = db('brand')->delete($id);
-      if($res){
-                $this->success('删除成功！','lst',1);
-            }else{
-                $this->error('删除失败！');
+        $cate=db('cate');
+        $cateTree=new Catetree();
+        $sonids=$cateTree->childrenids($id,$cate);
+        $sonids[]=intval($id);
+        $arrSys=[1,2,3];
+        $arrRes=array_intersect($arrSys,$sonids);
+        if($arrRes){
+            $this->error('系统内置文章分类不允许删除！');
+        }
+        //删除分类前判断该分类下的文章和文章相关缩略图
+        $article=db('article');
+        foreach ($sonids as $k => $v) {
+            $artRes=$article->field('id,thumb')->where(array('cate_id'=>$v))->select();
+            foreach ($artRes as $k1 => $v1) {
+                $thumbSrc=IMG_UPLOADS.$v1['thumb'];
+                if(file_exists($thumbSrc)){
+                    @unlink($thumbSrc);
+                }
+                $article->delete($v1['id']);
             }
+        }
+        $del=$cate->delete($sonids);
+        if($del){
+            $this->success('删除分类成功！','lst');
+        }else{
+            $this->error('删除分类失败！');
+        }
     }
 
     //上传图片
     public function upload(){
-    // 获取表单上传文件 例如上传了001.jpg
-    $file = request()->file('brand_img');
-    
-    // 移动到框架应用根目录/public/uploads/ 目录下
-    if($file){
-        $info = $file->move(ROOT_PATH . 'public' . DS . 'static'. DS .'uploads');
-        if($info){
-            return $info->getSaveName();
-        }else{
-            // 上传失败获取错误信息
-            echo $file->getError();
-            die;
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('brand_img');
+        
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'static'. DS .'uploads');
+            if($info){
+                return $info->getSaveName();
+            }else{
+                // 上传失败获取错误信息
+                echo $file->getError();
+                die;
+            }
         }
     }
-}
-
-
+    
 }
